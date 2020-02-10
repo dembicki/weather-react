9dd48d6207b6292489ccdde8c0db978f07a4d178
dembicki <nikeid1235@gmail.com>
2020-02-10 11:23:24 +0100

    created app structure with hard-coded values

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Weather = () => {
-    return (
-        <div className="contianer">
-            <div className="cards">
-                <h1>City name</h1>
-                <h5 className="py-4">
-                    <i className="wi wi-day-sunny display-1"></i>
-                </h5>
-            </div>
-        </div>
-    );
+  return (
+    <div className="contianer">
+      <div className="cards">
+        <h1>City name</h1>
+        <h5 className="py-4">
+          <i className="wi wi-day-sunny display-1"></i>
+        </h5>
+        <h1 className="py-2">25&deg;</h1>
+        {minMaxTemp(24, 19)}
+        <h4 className="py-3">Sunny</h4>
+      </div>
+    </div>
+  );
+};
+
+function minMaxTemp(min, max) {
+  return (
+    <h3>
+      <span className="px-4">{min}&deg;</span>
+      <span className="px-4">{max}&deg;</span>
+    </h3>
+  );
 }
 
 export default Weather;
