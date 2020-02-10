62ac6d7126f1267c651e4d4c8cc2c24f80e92760
dembicki <nikeid1235@gmail.com>
2020-02-10 11:15:06 +0100

    Added bootstrap and weather-icons packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import 'weather-icons/css/weather-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from './app_component/weather.component';
 
 function App() {
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Weather = () => {
     return (
         <div className="contianer">
-            <h1>City name/h1>
+            <div className="cards">
+                <h1>City name</h1>
+                <h5 className="py-4">
+                    <i className="wi wi-day-sunny display-1"></i>
+                </h5>
+            </div>
         </div>
     );
 }
