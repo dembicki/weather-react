9bb38fd0bb9f8d14e32855c2788cef7a8b188f32
dembicki <nikeid1235@gmail.com>
2020-02-10 11:06:25 +0100

    Weather component init

---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Weather = () => {
+    return (
+        <div className="contianer">
+            <h1>City name/h1>
+        </div>
+    );
+}
+
+export default Weather;
