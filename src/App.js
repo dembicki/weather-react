7db699b240b3880482295532d0834be9508410fd
dembicki <nikeid1235@gmail.com>
2020-02-10 12:13:52 +0100

    API connection fix

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import "./App.css";
 import "weather-icons/css/weather-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Weather from "./app_component/weather.component";
-
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`
+    );
+    const response = await api_call.json();
+    console.log(response);
+  };
+
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
