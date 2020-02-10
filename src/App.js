106c8635040604b7b62650135193593684d7f958
dembicki <nikeid1235@gmail.com>
2020-02-10 11:34:17 +0100

    Open Weather API config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import 'weather-icons/css/weather-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Weather from './app_component/weather.component';
+import "weather-icons/css/weather-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./app_component/weather.component";
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 function App() {
   return (
     <div className="App">
-     <Weather />
+      <Weather />
     </div>
   );
 }
-
-
-
 
 export default App;
